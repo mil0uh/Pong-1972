235726fdad31065249bc09b21d981abcedb35bd1
mil0uh <idrisskhelif@gmail.com>
2025-05-28 01:42:03 -0600

    Updated main.cpp <Currently testing rendering players>

--- Pong 1972/main.cpp
+++ Pong 1972/main.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <SDL3/SDL.h>
-
-
+#include "WindowMaker.h"
+using namespace std;
 
 
 int main() {
+	// Creating a 1280x720 window
+	WindowMaker window = WindowMaker("Pong 1972", 1280, 720);
 
-	SDL_Window* window;
-	SDL_Renderer* renderer;
+	Uint64 currentTime = SDL_GetTicks();
 
-	// Creating instance of 1280x720 window 
-	SDL_CreateWindowAndRenderer("Pong 1972", 1280, 720, SDL_WINDOW_OPENGL, &window, &renderer);
 	// Game loop condition
 	bool gameLoop = true;
-	// Variable to check for inputs such as closing the window
+
+	// Event Handler
 	SDL_Event event;
+
+	Player testObj = Player(200, 50, "Player");
+	Player testObj2 = Player(1080, 50, "Other Player");
 
 	while (gameLoop) {
 		
@@ -26,13 +29,18 @@ int main() {
 		}
 
 
+		window.clearWindow();
+		window.render(testObj);
+		window.render(testObj2);
+
+		window.present();
+	
+
 
 	}
 
-	SDL_DestroyWindow(window);
-	SDL_DestroyRenderer(renderer);
+	window.destroyWindow();
 
-	SDL_Quit();
 
 
 }
