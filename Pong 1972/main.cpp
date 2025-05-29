e1e4bc6930125c6d141e2419c6f752088c763b98
mil0uh <idrisskhelif@gmail.com>
2025-05-28 18:45:17 -0600

    Updated main class to show ball object and player movement

--- Pong 1972/main.cpp
+++ Pong 1972/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <SDL3/SDL.h>
 #include "WindowMaker.h"
+#include "Ball.h"
 using namespace std;
 
 
+
 int main() {
+
+
 	// Creating a 1280x720 window
 	WindowMaker window = WindowMaker("Pong 1972", 1280, 720);
 
@@ -16,8 +20,11 @@ int main() {
 	// Event Handler
 	SDL_Event event;
 
-	Player testObj = Player(200, 50, "Player");
-	Player testObj2 = Player(1080, 50, "Other Player");
+	Player Player1 = Player(100, 285, "Player");
+	Player Player2 = Player(1180, 285, "Other Player");
+	Ball myBall = Ball(640, 360, 30, 30);
+
+	int randomNumber = myBall.generateNumber(); // Generating random number in the range [0,1]
 
 	while (gameLoop) {
 		
@@ -30,9 +37,20 @@ int main() {
 
 
 		window.clearWindow();
-		window.render(testObj);
-		window.render(testObj2);
+		// Rendering Players and Ball
+		window.render(Player1);
+		window.render(Player2);
+		window.render(myBall,255,0,0,255);
 
+		// Checks randomly generated number and moves the ball left or right
+		myBall.startBall(randomNumber); 
+		// Moving player 1 and 2 while checking if they are within the 1280x720 boundary
+		Player1.movePlayer1();
+		Player1.boundaryChecker();
+		Player2.movePlayer2();
+		Player2.boundaryChecker();
+
+		// Drawing the objects
 		window.present();
 	
 
